b1e1f07f833d262cd523addcc5447b393ebc7886
antoinepdev <antoinepdeveloper@gmail.com>
2026-07-03 08:56:53 -0300

    refactor: move telegram querys into telegramRepository

--- src/services/fileService.ts
+++ src/services/fileService.ts
@@ -1,8 +1,8 @@
-import { bot } from "../bot.ts"
 import { ENV } from "../config/constants.ts"
+import { telegramRepository } from "../repositories/telegramRepository.ts"
 
 async function sendFile (destinataryId: number, message_id: number) {
-  await bot.api.copyMessage({chat_id: destinataryId, from_chat_id: ENV.PRIVATE_GROUP_ID!, message_id})
+  await telegramRepository.copyMessage(destinataryId, ENV.PRIVATE_GROUP_ID!, message_id)
 }
 
 export const fileService = {
