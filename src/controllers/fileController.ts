147245072e5cb50ddd8bd1094a4bb78184796b19
antoinepdev <antoinepdeveloper@gmail.com>
2026-07-03 01:24:04 -0300

    fix: validate file_id

--- src/controllers/fileController.ts
+++ src/controllers/fileController.ts
@@ -1,12 +1,20 @@
 import { bot } from "../bot.ts"
 import { fileService } from "../services/fileService.ts"
+import { ERROR } from "../config/constants.ts"
+import { errorHandler } from "../middlewares/errorHandler.ts"
 
 async function sendFileHandler () {
   bot.command("start", async (ctx) => {
-    const chat_id = ctx.chatId!
-    const file_id = ctx.match
+    try {
+      const chat_id = ctx.chatId!
+      const file_id = ctx.match
+      if (typeof(file_id) !== 'string' || file_id === '') throw new Error (ERROR.TELEGRAM.INVALID_LINK)
 
-    await fileService.sendFile(chat_id, file_id)
+      await fileService.sendFile(chat_id, file_id)
+    }
+    catch (error) {
+      errorHandler(error, ctx)
+    }
   })
 }
 
