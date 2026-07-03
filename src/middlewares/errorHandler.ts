a4976cd342b610f8ab1e64f78fd10be6ff29b83b
antoinepdev <antoinepdeveloper@gmail.com>
2026-07-03 08:56:53 -0300

    refactor: move telegram querys into telegramRepository

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,12 +1,13 @@
 import type { Context } from "node-telegram-bot-api"
 import { ERROR } from "../config/constants.ts"
+import { telegramRepository } from "../repositories/telegramRepository.ts"
 
 export async function errorHandler (error: unknown, ctx: Context ): Promise<void> {
   if (error instanceof Error) {
-    if (error.message === ERROR.TELEGRAM.INVALID_LINK) await ctx.reply('❌ Enlace inválido. Por favor, usa el enlace generado por el bot.')
+    if (error.message === ERROR.TELEGRAM.INVALID_LINK) await telegramRepository.replyMessage(ctx, '❌ Enlace inválido. Por favor, usa el enlace generado por el bot.')
     else {
       console.log(error.message)
-      await ctx.reply('Error interno del servidor. Intenta más tarde.')
+      await telegramRepository.replyMessage(ctx, '❌ Error interno del servidor. Intenta más tarde.')
     }
   }
 }
