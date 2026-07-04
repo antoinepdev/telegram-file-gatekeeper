2db6c01df0478db99db8d57f1f2f782984633e01
antoinepdev <antoinepdeveloper@gmail.com>
2026-07-04 13:37:48 -0300

    chore(entities): define Requirement

---
+++ src/entities/Requirement.ts
@@ -0,0 +1,9 @@
+export interface IRequirement {
+  id: string | number
+  type: 'telegram_channel'
+  target: string
+}
+
+export interface ICheckedRequirement extends IRequirement {
+  isSatisfied: boolean
+}
