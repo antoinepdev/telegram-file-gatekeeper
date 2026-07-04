295750c10a00b1632a065d44843d290a7f1a2c7e
antoinepdev <antoinepdeveloper@gmail.com>
2026-07-04 13:47:20 -0300

    chore(constants): define REQUERIMENTS

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,8 +1,12 @@
+import type { IRequirement } from "../entities/Requirement.ts"
+
 const env = process.env
 
 export const ENV = {
   BOT_TOKEN : env.BOT_TOKEN,
   PRIVATE_GROUP_ID : env.PRIVATE_GROUP_ID,
+  REQUIREMENT_ID: env.REQUIREMENT_ID,
+  REQUIREMENT_TARGET: env.REQUIREMENT_TARGET,
 }
 
 export const ERROR = {
@@ -10,3 +14,11 @@ export const ERROR = {
     INVALID_LINK: 'Invalid link'
   }
 }
+
+export const REQUIREMENTS: IRequirement[] = [
+  {
+    id: ENV.REQUIREMENT_ID!,
+    type: 'telegram_channel',
+    target: ENV.REQUIREMENT_TARGET!,
+  }
+]
